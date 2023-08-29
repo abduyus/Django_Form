49508e42c36126258d541d67013c0f67106e3f9e
abduyus <yusuf.abdurrasheed@outlook.com>
2023-08-29 18:28:33 +0000

    Make form in HTML (no implementation)

--- job_application/views.py
+++ job_application/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 
 def index(request):
+    if request.method == 'POST':
+        form = ContactForm()
+        first_name = form.
     return render(request, "index.html")
 
 
